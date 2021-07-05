4c472f71441e90332d7caa627112aa34ab5ffc36
issamghorbali <54804831+issamghorbali@users.noreply.github.com>
2021-07-05 08:49:46 +0200

    Update Question.php

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\QuestionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,6 +22,7 @@ class Question
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $title;
 
