9cb3d62b70b5703b1d2e4255417e0f306dbb6e47
issamghorbali <54804831+issamghorbali@users.noreply.github.com>
2021-07-05 08:50:17 +0200

    Update OptionQuestion.php

--- src/Entity/OptionQuestion.php
+++ src/Entity/OptionQuestion.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\OptionQuestionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,6 +22,7 @@ class OptionQuestion
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $title;
 
